ffbb84bb8dcacf2d06f27507db1d2ba800eacf79
Matthew Sullivan <msullivan@pacstar.com>
2017-11-22 16:51:51 -0800

    Added SPA win form for data management, implemented domain layer with entity framework to encapuslate data access for use in service and presentation. I think that this is where I am landing architecturally.

--- Presentation/Program.cs
+++ Presentation/Program.cs
@@ -16,7 +16,7 @@ namespace Bursty.Presentation
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new Main());
         }
     }
 }
